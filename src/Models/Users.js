5728882611e35d399cef08cc82d714b9b2571e65
tablonimus <lautamartin1995@gmail.com>
2023-10-03 21:53:32 -0300

    new users

--- src/Models/Users.js
+++ src/Models/Users.js
@@ -1,7 +1,30 @@
 const mongoose = require("mongoose");
 
 const UserSchemma = new mongoose.Schema({
-  name: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  identification_number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  cats: [{ type: mongoose.Schema.Types.ObjectId, ref: "Gatito" }],
 });
 
 const User = mongoose.model("User", UserSchemma);
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require("express"); //importo el modulo de express
 const cors = require("cors"); //importo el modulo de cors
 const app = express(); // lo ejecutamos y guardamos en una variable (guardamos una INSTANCIA de express)
 const port = 3000; // constante del puerto que levantare en el servidor
+//conectamos a mongo---
+require("./db.js"); /* ESTA LINEA ME CONECTA CON MONGO DB */
 
 // /* ------MIDDLEWARES (configuraciones express)---------------------------------------------------------- */
 app.use(express.json()); /*--Para aceptar json(body) en mis peticiones http-- */
@@ -12,7 +14,7 @@ app.use(cors()); /* Para aceptar peticiones del front o postman*/
 /* -------------------------- */
 // /* Vinculo mis modelos para usar rutas */
 const Gatito = require("./Models/Gatitos"); /* Conectamos el model correspondiente */
-const User = require ("./Models/Users")
+const User = require("./Models/Users");
 // /* RUTAS */
 
 app.get("/", (req, res) => {
@@ -22,17 +24,38 @@ app.get("/", (req, res) => {
 });
 
 app.post("/gatito", (req, res) => {
-  const name = req.query.name
+  const name = req.query.name;
   const nuevoGatito = Gatito.create({ name: name });
   res.json("Gato creado correctamente");
 });
 
-app.post("/user", (req, res) => {
-  
-  const name = req.query.name
-  const newUser = User.create({ name: name });
-  res.status(200).json(newUser);
+app.post("/user", async (req, res) => {
+  const {
+    username,
+    email,
+    identification_number,
+    password,
+    phone_number,
+    cats, // ["Roberto", "Esteban"]
+  } = req.body;
 
+  const catsFound = await Gatito.find({ name: { $in: cats } });
+
+  /* crear la instancia del usuario */
+  const user = new User({
+    username: username,
+    email: email,
+    identification_number: identification_number,
+    password: password,
+    phone_number: phone_number,
+    cats: catsFound.map((cat) => cat._id), //=> [new ObjectId("651cb3a8f62c611047c7be57"),new ObjectId("651cb3b5f62c611047c7be59"),]
+  });
+  /* Debemos encriptar la contraseña */
+
+  /* Guardo en la base de datos */
+  const newUser = user.save();
+
+  res.status(200).json("Usuario creado correctamente");
 });
 
 /* -------------------------------- */
